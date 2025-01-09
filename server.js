5cb310ee06884d3a58dae92c4d795f78bf0ef859
Megagig <megagigsoftwaresolutions.com>
2025-01-09 21:05:12 +0100

    Handliing 404 errors

--- server.js
+++ server.js
@@ -4,24 +4,29 @@ import authRoute from './routes/authRoute.js';
 
 dotenv.config();
 
-// import userRoute from './routes/userRoute';
-
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     success: true,
-//     message: 'Welcome to the Server',
-//   });
-// });
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to the Server',
+  });
+});
 
 //ALL ROUTES GO HERE
 
 app.use('/api/v1/auth', authRoute);
-// app.use('/api/v1/users', userRoute);
+
+//handle not found routes after all routes
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
