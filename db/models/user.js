17292255643d6cc202a5c163fb81936b3a8bed3d
Megagig <megagigsoftwaresolutions.com>
2025-01-12 17:30:02 +0100

    Validations, and Constraints in Sequelize Models

--- db/models/user.js
+++ db/models/user.js
@@ -14,24 +14,79 @@ const user = sequelize.define(
     },
     userType: {
       type: DataTypes.ENUM('0', '1', '2'),
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'firstName cannot be null',
+        },
+        notEmpty: {
+          msg: 'firstName cannot be empty',
+        },
+      },
     },
     firstName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'firstName cannot be null',
+        },
+        notEmpty: {
+          msg: 'firstName cannot be empty',
+        },
+      },
     },
     lastName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'lastName cannot be null',
+        },
+        notEmpty: {
+          msg: 'lastName cannot be empty',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
+
+      unique: {
+        msg: 'Email address already in use',
+      },
+
+      validate: {
+        notNull: {
+          msg: 'email cannot be null',
+        },
+        notEmpty: {
+          msg: 'email cannot be empty',
+        },
+        isEmail: {
+          msg: 'Invalid email id',
+        },
+      },
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'password cannot be null',
+        },
+        notEmpty: {
+          msg: 'password cannot be empty',
+        },
+      },
     },
-    // implement password hashing Logic
     confirmPassword: {
       type: DataTypes.VIRTUAL,
       set(value) {
-        if (value == this.password) {
+        if (this.password.length < 7) {
+          throw new AppError('Password length must be grater than 7', 400);
+        }
+        if (value === this.password) {
           const hashPassword = bcryptjs.hashSync(value, 10);
           this.setDataValue('password', hashPassword);
         } else {
